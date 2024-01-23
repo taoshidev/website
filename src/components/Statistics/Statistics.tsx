6588fb787aa2a4ad6512ffb4918beb4f221500aa
Raleigh, CA <knicholson@taoshi.io>
2024-01-23 14:35:08 -0500

    update statistics

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -1,27 +1,19 @@
-import { Box, HStack, Text, VStack } from "@chakra-ui/react";
+import { HStack, Text, VStack } from "@chakra-ui/react";
 
-export interface StatisticsProps {
-  prop?: string;
-}
-
-export const Statistics = ({ prop = "default value" }: StatisticsProps) => {
+export const Statistics = () => {
   return (
-    <HStack justify="space-between" alignItems="center" my="8">
-      <VStack spacing="0">
-        <Text fontWeight="bold">$345k</Text>
-        <Text fontSize="sm">Emissions</Text>
-      </VStack>
+    <HStack justify="space-around" alignItems="center" my="8">
       <VStack spacing="0">
         <Text fontWeight="bold">$1.3m</Text>
-        <Text fontSize="sm">Dividends</Text>
+        <Text fontSize="sm">Miner Payouts</Text>
+      </VStack>
+      <VStack spacing="0">
+        <Text fontWeight="bold">$345k</Text>
+        <Text fontSize="sm">Daily Mining Rewards</Text>
       </VStack>
       <VStack spacing="0">
         <Text fontWeight="bold">10/hr</Text>
-        <Text fontSize="sm">Payouts</Text>
-      </VStack>
-      <VStack spacing="0">
-        <Text fontWeight="bold">345k</Text>
-        <Text fontSize="sm">Emissions</Text>
+        <Text fontSize="sm">Payout Velocity</Text>
       </VStack>
     </HStack>
   );
