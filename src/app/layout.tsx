1c270f67e668b685a04ed340af8b2fa876c9f0d1
Raleigh, CA <knicholson@taoshi.io>
2024-01-25 11:52:40 -0500

    Add hubspot script tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,23 +7,33 @@ import { ADLaMDisplay, SpaceMono } from "@/styles/fonts";
 import { Providers } from "./providers";
 
 export const metadata: Metadata = {
-	title: "Taoshi",
-	description: "Taoshi - Predictions through the power of AI",
+  title: "Taoshi",
+  description: "Taoshi - Predictions through the power of AI",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en" className={clsx(ADLaMDisplay.variable, SpaceMono.variable)}>
-			<head>
-				<PlausibleProvider domain="taoshi.io" />
-			</head>
-			<body>
-				<Providers>{children}</Providers>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" className={clsx(ADLaMDisplay.variable, SpaceMono.variable)}>
+      <head>
+        <PlausibleProvider domain="taoshi.io" />
+      </head>
+      <body>
+        <Providers>{children}</Providers>
+
+        {/* <!-- Start of HubSpot Embed Code --> */}
+        <script
+          type="text/javascript"
+          id="hs-script-loader"
+          async
+          defer
+          src="//js.hs-scripts.com/45009699.js"
+        ></script>
+        {/* <!-- End of HubSpot Embed Code --> */}
+      </body>
+    </html>
+  );
 }
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,17 +1,14 @@
 "use client";
 
 import { ChakraProvider, CSSReset } from "@chakra-ui/react";
-import { HubspotProvider } from "next-hubspot";
 
 import { theme } from "@/styles/theme";
 
 export const Providers = ({ children }: any) => {
   return (
-    <HubspotProvider>
-      <ChakraProvider theme={theme}>
-        <CSSReset />
-        {children}
-      </ChakraProvider>
-    </HubspotProvider>
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+      {children}
+    </ChakraProvider>
   );
 };
