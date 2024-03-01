06470b52f51555f45fbc685ba66df25fa4521942
Raleigh, CA <knicholson@taoshi.io>
2024-03-01 13:24:10 -0500

    Return empty if error

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,14 +1,12 @@
 export async function getLeaderboard() {
-  const res = await fetch("http://34.65.161.184:48888/weights", {
-    cache: "no-store",
-  });
+  try {
+    const res = await fetch("http://34.65.161.184:48888/weights", {
+      cache: "no-store",
+    });
 
-  const data = await res.json();
+    const data = await res.json();
+    return JSON.parse(data);
+  } catch (error) {}
 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return JSON.parse(data);
+  return [];
 }
