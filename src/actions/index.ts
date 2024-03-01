2d7885991c99bfaa8c10d72d39bf852155e06cd8
Raleigh, CA <knicholson@taoshi.io>
2024-03-01 13:10:38 -0500

    Disable cache

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,8 @@
 export async function getLeaderboard() {
-  const res = await fetch("http://34.65.161.184:48888/weights");
+  const res = await fetch("http://34.65.161.184:48888/weights", {
+    cache: "no-store",
+  });
+
   const data = await res.json();
 
   if (!res.ok) {
