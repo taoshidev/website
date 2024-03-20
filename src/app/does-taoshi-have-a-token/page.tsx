f650e7d0b1355301b542f2c68dff40bf85608556
Raleigh, CA <knicholson@taoshi.io>
2024-03-20 01:23:36 -0400

    Add token page

---
+++ src/app/does-taoshi-have-a-token/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Box, Text, Anchor } from "@mantine/core";
+
+export default function Page() {
+	return (
+		<Box>
+			<Text>
+				No, its called{" "}
+				<Anchor
+					target="_blank"
+					href="https://youtu.be/dQw4w9WgXcQ?si=Xua9eUjQkCKrVIFj"
+				>
+					$TAO
+				</Anchor>
+				.
+			</Text>
+		</Box>
+	);
+}
