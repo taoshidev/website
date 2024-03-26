f9f9d1c0c79de9d474c7174a7c01112899b0ef16
Raleigh, CA <knicholson@taoshi.io>
2024-03-26 08:50:30 -0400

    Remove coming soon text

--- src/app/ptn/page.tsx
+++ src/app/ptn/page.tsx
@@ -8,206 +8,203 @@ import { Hero } from "@/components/Hero";
 import { Hubspot } from "@/components/PropNetHubspot";
 
 const ctas = [
-  {
-    event: "docs",
-    text: "Get Started Mining",
-    href: "https://github.com/taoshidev/proprietary-trading-network",
-    isExternal: true,
-  },
+	{
+		event: "docs",
+		text: "Get Started Mining",
+		href: "https://github.com/taoshidev/proprietary-trading-network",
+		isExternal: true,
+	},
 ];
 
 export default function Page() {
-  return (
-    <Box>
-      <Box>
-        <Text mb="xl" ta="center" size="xs" c="orange">
-          Coming March 22, 2024
-        </Text>
-        <Box mb="lg">
-          <PTNHero />
-        </Box>
+	return (
+		<Box>
+			<Box>
+				<Box mb="lg">
+					<PTNHero />
+				</Box>
 
-        <Box mb={200}>
-          <Hero
-            title="Taoshi's Proprietary Trading Network (PTN)"
-            ctas={ctas}
-            copy="Revolutionizing Proprietary Trading with Decentralized AI"
-          />
-        </Box>
-        <Card mb={75} style={{ border: "1px dashed black" }}>
-          <Text>
-            Taoshi&apos;s Proprietary Trading Network (PTN) spearheads a trading
-            revolution, merging cutting-edge decentralized technology with
-            sophisticated trading strategies. By leveraging the power of AI and
-            the collective intelligence of data scientists, traders, and machine
-            learning teams, PTN offers a unique synergy of proprietary trading,
-            decentralized AI, and quant trading signals, empowering all users to
-            navigate financial markets confidently.
-          </Text>
-        </Card>
-        <Box mb={75}>
-          <Text fw="bold" mb="sm">
-            How Taoshi&apos;s Proprietary Trading Network Functions
-          </Text>
-          <Text>
-            Traders, data scientists, and machine learning teams—all of whom are
-            &apos;miners&apos;—contribute their algorithmic or manual trading
-            signals across various financial markets, creating actionable
-            signals for clients. These signals are available on Taoshi&apos;s
-            Request Network, where users can purchase them based on their
-            trading needs. This collaborative ecosystem democratizes access to
-            high-level trading strategies and incentivizes miners through a
-            tokenized reward system, aligning the interests of all participants.
-          </Text>
-        </Box>
-        <Box mb={75}>
-          <Box mb="xl">
-            <Text fw="bold" mb="sm">
-              Why Mine or Purchase Signals on Proprietary Trading Network?
-            </Text>
-            <Text mb="md">
-              Taoshi&apos;s PTN leads the industry in decentralized proprietary
-              trading, and offers the following, unparalleled benefits for
-              miners and users of its outputs:
-            </Text>
-          </Box>
+				<Box mb={200}>
+					<Hero
+						title="Taoshi's Proprietary Trading Network (PTN)"
+						ctas={ctas}
+						copy="Revolutionizing Proprietary Trading with Decentralized AI"
+					/>
+				</Box>
+				<Card mb={75} style={{ border: "1px dashed black" }}>
+					<Text>
+						Taoshi&apos;s Proprietary Trading Network (PTN) spearheads a trading
+						revolution, merging cutting-edge decentralized technology with
+						sophisticated trading strategies. By leveraging the power of AI and
+						the collective intelligence of data scientists, traders, and machine
+						learning teams, PTN offers a unique synergy of proprietary trading,
+						decentralized AI, and quant trading signals, empowering all users to
+						navigate financial markets confidently.
+					</Text>
+				</Card>
+				<Box mb={75}>
+					<Text fw="bold" mb="sm">
+						How Taoshi&apos;s Proprietary Trading Network Functions
+					</Text>
+					<Text>
+						Traders, data scientists, and machine learning teams—all of whom are
+						&apos;miners&apos;—contribute their algorithmic or manual trading
+						signals across various financial markets, creating actionable
+						signals for clients. These signals are available on Taoshi&apos;s
+						Request Network, where users can purchase them based on their
+						trading needs. This collaborative ecosystem democratizes access to
+						high-level trading strategies and incentivizes miners through a
+						tokenized reward system, aligning the interests of all participants.
+					</Text>
+				</Box>
+				<Box mb={75}>
+					<Box mb="xl">
+						<Text fw="bold" mb="sm">
+							Why Mine or Purchase Signals on Proprietary Trading Network?
+						</Text>
+						<Text mb="md">
+							Taoshi&apos;s PTN leads the industry in decentralized proprietary
+							trading, and offers the following, unparalleled benefits for
+							miners and users of its outputs:
+						</Text>
+					</Box>
 
-          <Box mb="xl">
-            <Text mb="lg" fw={700}>
-              For Miners
-            </Text>
-            <List withPadding>
-              <List.Item mb="md">
-                <Text>
-                  <Text fw={700} component="span">
-                    World&apos;s Largest Incentivized Mining Pool for Trading
-                    Signals:
-                  </Text>{" "}
-                  PTN rewards miners with $TAO for their contributions,
-                  fostering a cycle of innovation.
-                </Text>
-              </List.Item>
-              <List.Item mb="md">
-                <Text>
-                  <Text fw={700} component="span">
-                    Diverse Financial Markets:
-                  </Text>{" "}
-                  Miners trade across various markets, leveraging AI to build
-                  sophisticated trading strategies.
-                </Text>
-                <AssetList />
-              </List.Item>
-              <List.Item mb="md">
-                <Text>
-                  <Text fw={700} component="span">
-                    Rapid Payouts:
-                  </Text>{" "}
-                  In traditional proprietary trading firms, traders often face
-                  rigorous challenges, sometimes spanning two phases. However,
-                  on PTN, miners can start receiving incentives right after
-                  their first trade.
-                </Text>
-              </List.Item>
-            </List>
-          </Box>
+					<Box mb="xl">
+						<Text mb="lg" fw={700}>
+							For Miners
+						</Text>
+						<List withPadding>
+							<List.Item mb="md">
+								<Text>
+									<Text fw={700} component="span">
+										World&apos;s Largest Incentivized Mining Pool for Trading
+										Signals:
+									</Text>{" "}
+									PTN rewards miners with $TAO for their contributions,
+									fostering a cycle of innovation.
+								</Text>
+							</List.Item>
+							<List.Item mb="md">
+								<Text>
+									<Text fw={700} component="span">
+										Diverse Financial Markets:
+									</Text>{" "}
+									Miners trade across various markets, leveraging AI to build
+									sophisticated trading strategies.
+								</Text>
+								<AssetList />
+							</List.Item>
+							<List.Item mb="md">
+								<Text>
+									<Text fw={700} component="span">
+										Rapid Payouts:
+									</Text>{" "}
+									In traditional proprietary trading firms, traders often face
+									rigorous challenges, sometimes spanning two phases. However,
+									on PTN, miners can start receiving incentives right after
+									their first trade.
+								</Text>
+							</List.Item>
+						</List>
+					</Box>
 
-          <Box mb="xl">
-            <Text mb="lg" fw={700}>
-              For Signal Users
-            </Text>
-            <List withPadding>
-              <List.Item mb="md">
-                <Text>
-                  <Text fw={700} component="span">
-                    Access to Highly Performant Trading Signals:
-                  </Text>{" "}
-                  Purchasing PTN&apos;s outputs grants access to
-                  institutional-grade insights, empowering users to make the
-                  most informed decisions. Institutions can leverage the vast
-                  network&apos;s collective intelligence to complement existing
-                  strategies, reduce research overheads, and enhance portfolio
-                  performance.
-                </Text>
-              </List.Item>
-              <List.Item mb="md">
-                <Text>
-                  <Text fw={700} component="span">
-                    Diverse Output Choices:
-                  </Text>{" "}
-                  Taoshi&apos;s Request Network provides a wide selection of
-                  PTN&apos;s outputs, allowing clients to tailor purchases to
-                  specific miners and financial markets.
-                </Text>
-              </List.Item>
-              <List.Item mb="md">
-                <Text>
-                  <Text fw={700} component="span">
-                    Transparent, Easy-to-Use Marketplace:
-                  </Text>{" "}
-                  The PTN marketplace operates with full transparency, ensuring
-                  the quality and performance of trading signals are openly
-                  available, fostering trust and reliability. Payments are
-                  simple; pay in stablecoins and start utilizing signals!
-                </Text>
-              </List.Item>
-            </List>
-          </Box>
-        </Box>
+					<Box mb="xl">
+						<Text mb="lg" fw={700}>
+							For Signal Users
+						</Text>
+						<List withPadding>
+							<List.Item mb="md">
+								<Text>
+									<Text fw={700} component="span">
+										Access to Highly Performant Trading Signals:
+									</Text>{" "}
+									Purchasing PTN&apos;s outputs grants access to
+									institutional-grade insights, empowering users to make the
+									most informed decisions. Institutions can leverage the vast
+									network&apos;s collective intelligence to complement existing
+									strategies, reduce research overheads, and enhance portfolio
+									performance.
+								</Text>
+							</List.Item>
+							<List.Item mb="md">
+								<Text>
+									<Text fw={700} component="span">
+										Diverse Output Choices:
+									</Text>{" "}
+									Taoshi&apos;s Request Network provides a wide selection of
+									PTN&apos;s outputs, allowing clients to tailor purchases to
+									specific miners and financial markets.
+								</Text>
+							</List.Item>
+							<List.Item mb="md">
+								<Text>
+									<Text fw={700} component="span">
+										Transparent, Easy-to-Use Marketplace:
+									</Text>{" "}
+									The PTN marketplace operates with full transparency, ensuring
+									the quality and performance of trading signals are openly
+									available, fostering trust and reliability. Payments are
+									simple; pay in stablecoins and start utilizing signals!
+								</Text>
+							</List.Item>
+						</List>
+					</Box>
+				</Box>
 
-        <Card style={{ border: "1px dashed black" }}>
-          <Text fw="bold" mb="sm">
-            Get Started: Mining on Proprietary Trading Network
-          </Text>
-          <Text>
-            Ready to contribute? Start your journey as a miner on PTN with our
-            README on GitHub. Starting Friday, March 22, 2024, follow our guide,
-            contribute signals, and earn rewards after closing your first trade.
-            We also recommend joining our Community Hub on Discord, a
-            collaborative space for miners.
-          </Text>
-        </Card>
+				<Card style={{ border: "1px dashed black" }}>
+					<Text fw="bold" mb="sm">
+						Get Started: Mining on Proprietary Trading Network
+					</Text>
+					<Text>
+						Ready to contribute? Start your journey as a miner on PTN with our
+						README on GitHub. Starting Friday, March 22, 2024, follow our guide,
+						contribute signals, and earn rewards after closing your first trade.
+						We also recommend joining our Community Hub on Discord, a
+						collaborative space for miners.
+					</Text>
+				</Card>
 
-        <Center my="xl">
-          <Divider
-            bg="black"
-            color="#F7F6F3"
-            orientation="vertical"
-            h={50}
-            variant="dashed"
-          />
-        </Center>
+				<Center my="xl">
+					<Divider
+						bg="black"
+						color="#F7F6F3"
+						orientation="vertical"
+						h={50}
+						variant="dashed"
+					/>
+				</Center>
 
-        <Image
-          mb={100}
-          h="auto"
-          w="100%"
-          fit="contain"
-          src="/flow.png"
-          alt="taoshi logo"
-        />
+				<Image
+					mb={100}
+					h="auto"
+					w="100%"
+					fit="contain"
+					src="/flow.png"
+					alt="taoshi logo"
+				/>
 
-        <Box mb={75}>
-          <Text fw="bold" mb="sm">
-            Interested in Purchasing PTN&apos;s Trading Outputs?
-          </Text>
-          <Text mb="sm">
-            Retail traders and institutional clients will find PTN&apos;s
-            outputs exceptionally valuable. With PTN, every trader and
-            institution can optimize their trading outcomes using AI.
-          </Text>
-          <Text>
-            If you&apos;re interested in purchasing the Trading Outputs
-            generated by PTN, please stay tuned as Taoshi&apos;s Request Network
-            is under development. We aim to release it in April 2024. Meanwhile,
-            we&apos;re providing signals via Telegram for free. Please stay
-            tuned and we&apos;ll provide a link soon!
-          </Text>
-        </Box>
-      </Box>
-      <Center my="xl">
-        <Divider variant="dashed" w="50%" bg="black" color="#F7F6F3" />
-      </Center>
-      <Hubspot />
-    </Box>
-  );
+				<Box mb={75}>
+					<Text fw="bold" mb="sm">
+						Interested in Purchasing PTN&apos;s Trading Outputs?
+					</Text>
+					<Text mb="sm">
+						Retail traders and institutional clients will find PTN&apos;s
+						outputs exceptionally valuable. With PTN, every trader and
+						institution can optimize their trading outcomes using AI.
+					</Text>
+					<Text>
+						If you&apos;re interested in purchasing the Trading Outputs
+						generated by PTN, please stay tuned as Taoshi&apos;s Request Network
+						is under development. We aim to release it in April 2024. Meanwhile,
+						we&apos;re providing signals via Telegram for free. Please stay
+						tuned and we&apos;ll provide a link soon!
+					</Text>
+				</Box>
+			</Box>
+			<Center my="xl">
+				<Divider variant="dashed" w="50%" bg="black" color="#F7F6F3" />
+			</Center>
+			<Hubspot />
+		</Box>
+	);
 }
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -6,72 +6,72 @@ import { create, set, render, frame, globe } from "@/utils/logo";
 import styles from "./logo.module.css";
 
 interface LogoProps {
-  size?: number;
+	size?: number;
 }
 
-export const Logo = ({ size = 200 }: LogoProps) => {
-  const [filler, setFiller] = useState("");
-  const [content, setContent] = useState("");
-  const hiddenRef = useRef<HTMLDivElement | null>(null);
-  const rootRef = useRef<HTMLDivElement | null>(null);
+export const Logo = ({ size = 500 }: LogoProps) => {
+	const [filler, setFiller] = useState("");
+	const [content, setContent] = useState("");
+	const hiddenRef = useRef<HTMLDivElement | null>(null);
+	const rootRef = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    const fillerSize = size / 2;
-    const initialFiller =
-      "\u28ff".repeat(fillerSize) + "\n\u28ff".repeat(fillerSize - 1);
-    setFiller(initialFiller);
+	useEffect(() => {
+		const fillerSize = size / 2;
+		const initialFiller =
+			"\u28ff".repeat(fillerSize) + "\n\u28ff".repeat(fillerSize - 1);
+		setFiller(initialFiller);
 
-    const animate = async () => {
-      for (;;) {
-        await frame();
+		const animate = async () => {
+			for (;;) {
+				await frame();
 
-        if (!hiddenRef.current || !rootRef.current) continue;
+				if (!hiddenRef.current || !rootRef.current) continue;
 
-        const hr = hiddenRef.current.getBoundingClientRect();
-        const fontWidth = hr.width / fillerSize;
-        const fontHeight = hr.height / fillerSize;
+				const hr = hiddenRef.current.getBoundingClientRect();
+				const fontWidth = hr.width / fillerSize;
+				const fontHeight = hr.height / fillerSize;
 
-        const rr = rootRef.current.getBoundingClientRect();
-        const ratio = rr.width / rr.height;
-        const width = (rr.width / fontWidth) << 1;
-        const height = (rr.height / fontHeight) << 2;
+				const rr = rootRef.current.getBoundingClientRect();
+				const ratio = rr.width / rr.height;
+				const width = (rr.width / fontWidth) << 1;
+				const height = (rr.height / fontHeight) << 2;
 
-        const pixels: number[][] = Array.from(create(width, height), (row) =>
-          Array.from(row)
-        );
-        const time = 1e-3 * Date.now();
+				const pixels: number[][] = Array.from(create(width, height), (row) =>
+					Array.from(row)
+				);
+				const time = 1e-3 * Date.now();
 
-        for (let y = 0; y < height; y++) {
-          const v = (2 * y) / height - 1;
+				for (let y = 0; y < height; y++) {
+					const v = (2 * y) / height - 1;
 
-          for (let x = 0; x < width; x++) {
-            const u = ratio * ((2 * x) / width - 1);
+					for (let x = 0; x < width; x++) {
+						const u = ratio * ((2 * x) / width - 1);
 
-            if (globe(x, y, u, v, time)) {
-              set(pixels, x, y);
-            }
-          }
-        }
+						if (globe(x, y, u, v, time)) {
+							set(pixels, x, y);
+						}
+					}
+				}
 
-        setContent(render(pixels));
-      }
-    };
+				setContent(render(pixels));
+			}
+		};
 
-    animate();
-  }, []);
+		animate();
+	}, []);
 
-  return (
-    <Center c="black" mb="xl">
-      <Box ref={rootRef} className={styles.braille} w={size} h={size}>
-        <Box
-          ref={hiddenRef}
-          className={styles.hidden}
-          style={{ visibility: "hidden" }}
-        >
-          {filler}
-        </Box>
-        <Box>{content}</Box>
-      </Box>
-    </Center>
-  );
+	return (
+		<Center c="black" mb="xl">
+			<Box ref={rootRef} className={styles.braille} w={size} h={size}>
+				<Box
+					ref={hiddenRef}
+					className={styles.hidden}
+					style={{ visibility: "hidden" }}
+				>
+					{filler}
+				</Box>
+				<Box>{content}</Box>
+			</Box>
+		</Center>
+	);
 };
